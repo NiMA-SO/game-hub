2e703820ce5429bd0ead69d71b97bc74950c261c
NIMA <nima85sohrabi@gmail.com>
2024-07-30 19:37:18 +0330

    ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../Data/genres";
 
 
 export interface Genre{
@@ -7,7 +7,8 @@ export interface Genre{
     image_background: string;
 }
 
- const useGenres = () => useData<Genre>('/genres');
+//  const useGenres = () => useData<Genre>('/genres');
+ const useGenres = () => ({ data: genres , isLoading: false, error: null });
 
 
 export default useGenres;
